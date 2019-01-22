27e8d8f63b207fdb0e8e5b6aba111a7dd8532786
Balaji <bala.89ji@gmail.com>
2019-01-22 14:07:50 +0530

    css_issue_fix_p3

--- iron-list.js
+++ iron-list.js
@@ -271,12 +271,6 @@ Polymer$0({
         display: block;
       }
 
-      @media only screen and (-webkit-max-device-pixel-ratio: 1) {
-        :host {
-          will-change: transform;
-        }
-      }
-
       #items {
         @apply --iron-list-items-container;
         position: relative;
@@ -291,7 +285,6 @@ Polymer$0({
         margin: 0;
         position: absolute;
         top: 0;
-        will-change: transform;
       }
     </style>
 
